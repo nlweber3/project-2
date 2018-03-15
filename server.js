ea1939729a56f67d176c6634b110641f30a09983
Nick <nlweber3@gmail.com>
2018-03-14 18:16:53 -0700

    changed schema testing branch

--- server.js
+++ server.js
@@ -17,7 +17,6 @@ app.set("view engine", "handlebars");
 
 // server listener
 app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
+  console.log("App listening on PORT " + PORT);
+});
 
-  
