be1101ee5a6fcfb39bedc997df06d5a9f454e234
Nick <nlweber3@gmail.com>
2018-03-19 19:06:43 -0700

    added database

---
+++ app/config/database.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+
+module.exports = {
+    'connection': {
+        'host': 'localhost',
+        'user': 'root', 
+        'password': process.env.PASSWORD,
+        'database': 'db'
+    },
+	'database': 'db',
+    
+};
